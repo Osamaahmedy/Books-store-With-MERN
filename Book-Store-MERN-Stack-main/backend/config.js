bc44a67fbf3eba0b1fb7a10b04f4bea00c97da59
Osamaahmedy <168088139+Osamaahmedy@users.noreply.github.com>
2024-09-01 05:27:43 -0700

    first

---
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  
+'mongodb+srv://root:root@books-store-merm.s9dld.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERM'
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
